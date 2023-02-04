877d0a8c869161ece7ef084de7b453cd4c427f49
MarceAriel99 <mrondan@fi.uba.ar>
2023-02-04 13:27:47 -0300

    Improved raycast script and small changes to the car

--- Assets/Scripts/Raycasts.cs
+++ Assets/Scripts/Raycasts.cs
@@ -3,63 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Raycasts : MonoBehaviour
-{
-    GameObject car;
+{   
+    public bool show_raycast_line;
+    public bool debug_raycast_console;
+
+    public float length = 50f;
+
+    Transform car;
+    RaycastHit hit;
+    bool raycast_hitted = false;
+
+    Vector3 origin;
+    Vector3[] directions = new Vector3[8];
+
     void Start()
-    {
-        car = GameObject.Find("COM");
+    {   
+        // Get car's transform
+        car = transform;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // set raycast origin to car's center of mass
-        Vector3 origin = car.transform.position + new Vector3(0, 0.5f, 0);
-        // set raycast directions to car's N NE E SE S SW W NW
-        Vector3[] directions = new Vector3[8];
-        directions[0] = car.transform.forward;
-        directions[1] = car.transform.forward + car.transform.right;
-        directions[2] = car.transform.right;
-        directions[3] = car.transform.right - car.transform.forward;
-        directions[4] = -car.transform.forward;
-        directions[5] = -car.transform.forward - car.transform.right;
-        directions[6] = -car.transform.right;
-        directions[7] = -car.transform.right + car.transform.forward;
+        // Set raycast origin to car's center
+        origin = car.position + new Vector3(0, 0.5f, 0);
+
+        // Set raycast directions to car's N NE E SE S SW W NW
+        directions[0] = car.forward;
+        directions[1] = car.forward + car.right;
+        directions[2] = car.right;
+        directions[3] = car.right - car.forward;
+        directions[4] = -car.forward;
+        directions[5] = -car.forward - car.right;
+        directions[6] = -car.right;
+        directions[7] = -car.right + car.forward;
 
         // Normalize directions
         for (int i = 0; i < 8; i++)
         {
             directions[i] = directions[i].normalized;
         }
-
-        float length = 50f;
-        bool raycast_hitted = false;
-
-        // iterate from 0 to 7
+        
+        // Iterate through raycasts
         for (int i = 0; i < 8; i++)
         {
-            RaycastHit hit;
-
             // Cast ray 
             raycast_hitted =  Physics.Raycast(origin, directions[i], out hit, length);
 
             // TODO: send data to neural network
             
             // Debug raycasts
-            //DebugRaycast(origin, directions[i], length, hit, i, raycast_hitted);
+            if (show_raycast_line)
+                DrawRaycast(origin, directions[i], length, hit, i, raycast_hitted);
+            if (debug_raycast_console)
+                LogRaycast(origin, directions[i], length, hit, i, raycast_hitted);
         }
     }
 
-    private void DebugRaycast(Vector3 origin, Vector3 direction, float length, RaycastHit hit, int raycast_number, bool raycast_hitted)
+    private void DrawRaycast(Vector3 origin, Vector3 direction, float length, RaycastHit hit, int raycast_number, bool raycast_hitted)
     {
         if (raycast_hitted && hit.collider.gameObject.tag == "Wall")
         {
             Debug.DrawRay(origin, direction * hit.distance, Color.yellow);
-            Debug.Log("Raycast number " + raycast_number + " did hit " + hit.collider.gameObject.name + " at " + hit.point + " with distance " + hit.distance + " and tag " + hit.collider.gameObject.tag);
         }
         else
         {
             Debug.DrawRay(origin, direction * length, Color.white);
+        }
+    }
+
+    private void LogRaycast(Vector3 origin, Vector3 direction, float length, RaycastHit hit, int raycast_number, bool raycast_hitted)
+    {
+        if (raycast_hitted && hit.collider.gameObject.tag == "Wall")
+        {
+            Debug.Log("Raycast number " + raycast_number + " did hit " + hit.collider.gameObject.name + " at " + hit.point + " with distance " + hit.distance + " and tag " + hit.collider.gameObject.tag);
+        }
+        else
+        {
             Debug.Log("Raycast number " + raycast_number + " did not hit anything");
         }
     }
